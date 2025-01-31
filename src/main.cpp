3f9e2bcd86cd7d3e8a915ad32c43505256865bd4
Frits Jan van Kempen <fritsjan@users.noreply.github.com>
2025-01-31 14:21:53 +0100

    working with steps and dual heartbeats

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,27 @@ bool ledState = LOW;
 Servo servoA;
 Servo servoB;
 
-BlockNot servoSpeed(20);
-BlockNot startTimerA(1000);
-BlockNot startTimerB(1000);
+BlockNot servoSpeed(22);
+BlockNot startTimerA(1200);
+BlockNot startTimerB(100); // time between A and B
+BlockNot changeBeat(20000);
 
 // Published values for SG90 servos; adjust if needed
 int minUs = 500;
 int maxUs = 2500;
 
 int minPosA = 0;
-int maxPosA = 90;
 int minPosB = 0;
+int maxPosA = 90;
 int maxPosB = 90;
 bool invertA = false;
 bool invertB = true;
 
 int posA = minPosA;
-int posAstep = 10;
-bool dirA = true; // true is 0 -> 180
 int posB = minPosB;
+int posAstep = 10;
 int posBstep = 10;
+bool dirA = true; // true is 0 -> 180
 bool dirB = true; // true is 0 -> 180
 
 int servoModeA = 0;
@@ -46,6 +47,7 @@ int servoModeB = 0;
 ESP32PWM pwm;
 
 bool run = true;
+bool beat = true; // switch between 2 different beats
 
 void processSerial() {
     String input = Serial.readString();
@@ -53,10 +55,15 @@ void processSerial() {
         run = !run;
         Serial.println((run ? "Loop RUNNING" : "Loop PAUSED"));
     }
-    if (input.startsWith("s")) {
-        servoModeA = 1;
-        servoModeB = 1;
-        Serial.println("StartA");
+    if (input.startsWith("q")) {
+        maxPosA = 160;
+        maxPosB = 160;
+        Serial.println("larger");
+    }
+    if (input.startsWith("w")) {
+        maxPosA = 90;
+        maxPosB = 90;
+        Serial.println("normal");
     }
     while(Serial.available()) Serial.read();
 }
@@ -76,6 +83,8 @@ void setup() {
   servoB.attach(servo2Pin, minUs, maxUs);
   pinMode(LED, OUTPUT);
 
+  startTimerB.stop();
+
 }
 
 
@@ -83,78 +92,92 @@ void loop() {
 
   if (Serial.available()) processSerial();
 
-  if(startTimerA.triggered()) { servoModeA = 1;}
-  if(startTimerB.triggered()) { servoModeB = 1;}
-
   if(run){
 
-    if(servoSpeed.triggered()){
+    if(startTimerA.triggered()) { 
+      startTimerB.start(WITH_RESET); // reset and start delay for timer B
+      servoModeA = 10; // start servoA
+      }
+    if(startTimerB.firstTrigger()) { 
+      servoModeB = 10; // start servoB
+      }
+    
+    if(changeBeat.triggered()){
+      beat = !beat;
+      if (beat){
+        maxPosA = 100;
+        maxPosB = 100;
+      } else {
+        maxPosA = 140;
+        maxPosB = 140;
+      }
+    }
+
+    if(servoSpeed.triggered()){ // update on servoSpeed timer fired
 
       switch(servoModeA){
         case 0:
-        // init
-          posA = 0;
-          //servoModeA = 1;
-          break;
-        case 1:
-          //move from 0 - 90
-          posA += posAstep;
-          if (posA >= maxPosA) { posA = maxPosA; servoModeA = 2; }
-          if (invertA) { servoA.write(180 - posA); } else { servoA.write(posA); }
-          break;
-        case 2:
-          //move from 90 - 0
-          posA -= posAstep;
-          if (posA <= minPosA) { posA = minPosA; servoModeA = 3;} //Serial.println("go idle");}
-          if (invertA) { servoA.write(180 - posA); } else { servoA.write(posA); }
-          break;
-        case 3: 
           // idle
           break;
+        case 10:
+          // start
+          posA = 0;
+          // go move
+          servoModeA = 20;
+          break;
+        case 20:
+          //move from 0 - 90
+          posA += posAstep;
+          if (posA >= maxPosA) { posA = maxPosA; servoModeA = 30; } // go back
+          if (invertA) { servoA.write(180 - posA); } else { servoA.write(posA); }
+          break;
+        case 30:
+          //move from 90 - 0
+          posA -= posAstep;
+          if (posA <= minPosA) { posA = minPosA; servoModeA = 0;} //go idle
+          if (invertA) { servoA.write(180 - posA); } else { servoA.write(posA); }
+          break;
+
       }
 
       switch(servoModeB){
         case 0:
-        // init
-          posB = 0;
-          //servoModeB = 1;
+          // idle
           break;
-        case 1:
+        case 10:
+          //init
+          posB = 0;
+          servoModeB = 20;
+          break;
+        case 20:
           //move from 0 - 90
           posB += posBstep;
-          if (posB >= maxPosB) { posB = maxPosB; servoModeB = 2; }
+          if (posB >= maxPosB) { posB = maxPosB; servoModeB = 30; } // go back
           if (invertB) { servoB.write(180 - posB); } else { servoB.write(posB); }
           break;
-        case 2:
+        case 30:
           //move from 90 - 0
           posB -= posBstep;
-          if (posB <= minPosB) { posB = minPosB; servoModeB = 3;} //Serial.println("go idle");}
+          if (posB <= minPosB) { posB = minPosB; servoModeB = 0;} // go idle
           if (invertB) { servoB.write(180 - posB); } else { servoB.write(posB); }
-          break;
-        case 3: 
-          // idle
           break;
       }
 
     }
 
+  } else {
 
+      int wait = 2000;
 
-    // // non blocking servo motion
-    // if(servoSpeed.triggered()){
-    //   // increase or decrease position
-    //   if (dirA) { posA += posAstep; } else { posA -= posAstep; }
-    //   // check position and set direction
-    //   if (posA >= maxPosA) { posA = maxPosA; dirA = false;  }
-    //   if (posA <= minPosA) { posA = minPosA; dirA = true;   }
-    //   // update position of servo
-    //   if (invertA) { servoA.write(180 - posA); } else { servoA.write(posA); }
-    //   if (invertB) { servoB.write(180 - posA); } else { servoB.write(posA); }
-    // }
-
+      servoA.write(90);
+      delay(wait);
+      servoA.write(180);
+      delay(wait);
+      servoA.write(90);
+      delay(wait);
+      servoA.write(0);
+      delay(wait);
   }
-
-
 
   // servoSpeed.setDuration(10); // update speed of servo movement
 
